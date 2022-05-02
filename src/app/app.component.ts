e50c14654ced25ae8cbb6b70b91d182693931ea4
Camilo Rozo <camilo.rozo@ceiba.com.co>
2022-05-02 17:24:26 -0500

    reducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {decrement, increment, reset} from './shared/counter.actions';
+import {Observable} from 'rxjs';
+
+interface AppState {
+  count: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux';
+  number: number;
+  count$: Observable<number>;
+
+  constructor(private store: Store<AppState>) {
+    this.store.subscribe(state => {
+      this.number = state.count;
+      console.log(state.count);
+    });
+    this.count$ = store.select('count');
+  }
+
+  increment(): void {
+    this.store.dispatch(increment());
+  }
+
+  decrement(): void {
+    this.store.dispatch(decrement());
+  }
+
+  reset(): void {
+    this.store.dispatch(reset());
+  }
+
+
 }
